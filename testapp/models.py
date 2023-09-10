dc781ad57340a79793160f4a8ec7a14c14293ede
Pro7Prabhakar <prabhashankarmishra202@gmail.com>
2023-09-10 11:46:54 +0530

    branch-1 with some changes

--- testapp/models.py
+++ testapp/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Movie(models.Model):
-    release_date = models.DateField()
-    movie_name = models.CharField(max_length=40)
+    rdate = models.DateField()
+    moviename = models.CharField(max_length=40)
     hero = models.CharField(max_length=20)
     heroine = models.CharField(max_length=20)
     rating = models.IntegerField()
